ec013ee411a6123525c40cd18b029f58fef6ec71
Keshani Balasooriya <youremail@example.com>
2025-11-14 16:16:45 +0530

    Updated backend with body-parser and mongoose

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
+import bodyParser from 'body-parser';
 import express from 'express';
+import mongoose from 'mongoose';
+
 
 const app = express();
 const PORT = 3000;      
 
 const mongoDB_url='mongodb+srv://admin:1234@cluster0.5tomp5w.mongodb.net/?appName=Cluster0';
-;
 
-app.get('/', () => {
-  console.log('Hello, World!');
+mongoose.connect(mongoDB_url);
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully');
 });
+
+
+
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    console.log('Request received at /');
+    res.send("Hello");
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
