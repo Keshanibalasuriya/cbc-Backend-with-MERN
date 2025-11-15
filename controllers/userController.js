d1d053cb2bb8af37c271227590292abfbe847ca6
Keshani Balasuriya <your-correct-email@gmail.com>
2025-11-15 10:16:35 +0530

    Password Hashing Added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,19 @@
 import User from "../models/user.js";
-
+import bcrypt from  "bcrypt";
 
 
 export function getUsers(req, res) {
 
+    //password hashing
+    const newUserData=req.body;
+    newUserData.password=bcrypt.hashSync(newUserData.password,10);
+    console.log("Hashed Password:",newUserData.password);
+
+
+    //creating a new user
     const user = new User(req.body);
     user.save()
         .then(() => res.json({ message: 'User created successfully!' }))
         .catch(err => res.status(400).json({ error: err.message }));
 }
+
