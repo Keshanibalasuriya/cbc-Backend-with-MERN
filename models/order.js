882b86735c3ad9f7546f1f4d3637fea562fd0765
Keshani Balasuriya <your-correct-email@gmail.com>
2025-11-17 20:25:02 +0530

    Added Order model

---
+++ models/order.js
@@ -0,0 +1,62 @@
+import { HostAddress } from "mongodb";
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+    orderID: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true 
+    },
+    orderedItems: [
+        { 
+            name: {
+                type: String,
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true
+            },
+            image: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    paymentId: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        default: "Preparing"
+    },
+    name: { 
+        type: String, 
+        required: true 
+    },
+    address: { 
+        type: String, 
+        required: true
+    },
+    phone: { 
+        type: String, 
+        required: true 
+    }
+
+});
+
+
+
+
+const Order = mongoose.model("Orders", orderSchema);
+
+export default Order;
--- models/product.js
+++ models/product.js
@@ -1,18 +1,39 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    productID:{
-         type: String,
-          required: true,
-           unique: true 
-        },
-    ProductName:{ type: String, required: true },
-    altNames:{ type: String },
-    Image:{ type: String },
-    price:{ type: Number, required: true },
-    lastPrice:{ type: Number ,required: true },
-stock:{ type: Number ,required: true },
-    description:{ type: String ,required: true},
+    productID: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    productName: {
+        type: String,
+        required: true
+    },
+    altNames: {
+        type: String,
+        required: false
+    },
+    image: {
+        type: String,
+        required: false
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    lastPrice: {
+        type: Number,
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
 });
 
 const Product = mongoose.model("Products", productSchema);
